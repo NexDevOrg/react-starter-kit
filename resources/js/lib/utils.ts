fb0a96c0f2fc5e0f2abd74a1433cc63c61daaec7
Finn <finn.groenewoud@gmail.com>
2025-03-25 13:43:17 +0100

    Add ucfirst utility function to capitalize the first letter of a string in utils.ts

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function ucfirst(string: string): string {
+    if (!string) return string;
+
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
